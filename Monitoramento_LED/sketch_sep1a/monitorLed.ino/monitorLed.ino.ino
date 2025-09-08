f9bfb21af2e09c07c0b85f093f252c30a5b35a27
HyagoGabriel <hyagogabriel3@gmail.com>
2025-09-08 11:18:44 -0300

    BOTAO_ALARME

--- Monitoramento_LED/sketch_sep1a/monitorLed.ino/monitorLed.ino.ino
+++ Monitoramento_LED/sketch_sep1a/monitorLed.ino/monitorLed.ino.ino
@@ -1,55 +1,80 @@
-#define pinoLed 14
+
+
+#define BUZZER_PIN 27
+#define LED_ALARME 13
+#define BOTAO_FISICO 26
+#define TRIG_PIN 12
+#define ECHO_PIN 14
+
+
 
 #include <math.h>
 #include "AdafruitIO_WiFi.h"
+#include "NewPing.h"
 
 // Configuração do WiFi
-#define WIFI_SSID "fogazzaaaa"
-#define WIFI_PASS "fogazzaaaa"
+// #define WIFI_SSID "fogazzaaaa"
+// #define WIFI_PASS "fogazzaaaa"
 
-// Autenticação AdafuritIO
-#define IO_USERNAME  "Hyago"
-#define IO_KEY       "aio_rYgO747Cr7u9wFxWojDhYlaAK6kb"
+// // Autenticação AdafuritIO
+// #define IO_USERNAME "Hyago"
+// #define IO_KEY "aio_rYgO747Cr7u9wFxWojDhYlaAK6kb"
 
-AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+// AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
-AdafruitIO_Feed *botaoled = io.feed("botaoled");
+// AdafruitIO_Feed *botaoled = io.feed("botaoled");
+
+//configuraçao do ultrassonico
+#define MAX_DISTANCE 100
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
 
-  pinMode(pinoLed, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_ALARME, OUTPUT);
+  pinMode(BOTAO_FISICO, INPUT);
+
   Serial.begin(115200);
 
   while (!Serial)
     ;
+    delay(1000);
 
-  Serial.println("Conectando com AdafruitIO");
-  io.connect();
+  // Serial.println("Conectando com AdafruitIO");
+  // io.connect();
 
-  int numTentativas = 0;
+  // int numTentativas = 0;
 
-  while (io.status() < AIO_CONNECTED) {
-    Serial.print(numTentativas);
-    Serial.println(" - Conexão Ainda Não Realizada");
-    numTentativas = numTentativas + 1;
-    delay(500);
-  }
+  // while (io.status() < AIO_CONNECTED) {
+  //   Serial.print(numTentativas);
+  //   Serial.println(" - Conexão Ainda Não Realizada");
+  //   numTentativas = numTentativas + 1;
+  //   delay(500);
+  // }
 
-  // Mostra status da conexão
+  // // Mostra status da conexão
 
-  Serial.println();
-  Serial.println(io.statusText());
+  // Serial.println();
+  // Serial.println(io.statusText());
 
-  // Configuração do Callback, quando o feed recebe(atualizar) um valor
+  // // Configuração do Callback, quando o feed recebe(atualizar) um valor
 
-  botaoled->onMessage(handleBotaoLed);  // Registra a função de Callback
+  // botaoled->onMessage(handleBotaoLed);  // Registra a função de Callback
 
-  delay(1000);
+  // delay(1000);
 }
 
 void loop() {
 
-  io.run();  // Manter a conexão com a AdrafuitIO Ativa
+  Serial.print(F("Distancia lida: "));
+  Serial.println(sonar.ping_cm());
+  delay(500);
 
-  //delay(3000);
+  // testeLed();
+  // testeBuzzer();
+  // testeButton(BOTAO_FISICO);
+
+  // io.run();  // Manter a conexão com a AdrafuitIO Ativa
+
+  // //delay(3000);
 }
